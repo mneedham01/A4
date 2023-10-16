ede0d84a5faeff6c68d94edb16ef1f23d502fc61
mneedham01 <mneedham@smith.edu>
2023-10-16 19:59:59 -0400

    deleted print statements

--- SolveMaze.java
+++ SolveMaze.java
@@ -3,12 +3,8 @@ class SolveMaze {
 
   /** */
   public boolean checkLocation (Maze maze, MazeLocation current) {
-
-    System.out.println("Called checkLocation on square: "+ current.getRow() + current.getCol());
-
     // if current is final location in maze
     if (current.equals(maze.getFinish())) {
-      System.out.println("Current = finish.");
       // mark current as part of path
       maze.grid[current.getRow()][current.getCol()] = MazeContents.PATH;
       return true;
@@ -16,11 +12,9 @@ class SolveMaze {
     MazeContents contentsAtCurrent = maze.getContents(current.getRow(), current.getCol());
     // if current is a wall or already visited
     if (contentsAtCurrent.equals(MazeContents.VISITED) || contentsAtCurrent.equals(MazeContents.WALL)) {
-      System.out.println("Current = wall OR Current = already visited.");
       return false;
     }
     else {
-      System.out.println("In the else.");
       maze.grid[current.getRow()][current.getCol()] = MazeContents.VISITED;;
       try { Thread.sleep(50);	} catch (InterruptedException e) {};
       if (checkLocation(maze, current.neighbor(MazeDirection.NORTH)) || checkLocation(maze, current.neighbor(MazeDirection.SOUTH)) || checkLocation(maze, current.neighbor(MazeDirection.EAST)) || checkLocation(maze, current.neighbor(MazeDirection.WEST))) {
