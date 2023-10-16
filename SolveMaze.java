04c777f35ac912e181299584079e7a35fbc14e76
mneedham01 <mneedham@smith.edu>
2023-10-16 14:34:52 -0400

    working solveMaze

--- SolveMaze.java
+++ SolveMaze.java
@@ -14,18 +14,21 @@ class SolveMaze {
 
     // if current is final location in maze
     if (current.equals(maze.getFinish())) {
+      System.out.println("Current = finish.");
       // mark current as part of path
       current.setOnPath(true);
       return true;
     }
     // if current is a wall or already visited
-    if (current.getVisited()) {
+    if (current.getVisited() || maze.getContents(current.getRow(), current.getCol()).equals(MazeContents.WALL)) {
+      System.out.println("Current = wall OR Current = already visited.");
       return false;
     }
     else {
+      System.out.println("In the else.");
       current.setVisited(true);
       try { Thread.sleep(50);	} catch (InterruptedException e) {};
-      if (checkLocation(maze, current.neighbor(NORTH)) || checkLocation(maze, current.neighbor(SOUTH)) || checkLocation(maze, current.neighbor(EAST)) || checkLocation(maze, current.neighbor(WEST))) {
+      if (checkLocation(maze, current.neighbor(MazeDirection.NORTH)) || checkLocation(maze, current.neighbor(MazeDirection.SOUTH)) || checkLocation(maze, current.neighbor(MazeDirection.EAST)) || checkLocation(maze, current.neighbor(MazeDirection.WEST))) {
         current.setOnPath(true);
       }
     }
@@ -38,7 +41,9 @@ class SolveMaze {
     MazeViewer viewer = new MazeViewer(maze);
     SolveMaze solve = new SolveMaze();
 
-    solve.checkLocation(maze, maze.getStart());
+    if (solve.checkLocation(maze, maze.getStart())) {
+      System.out.println("Found solution");
+    }
 
   }
 }
