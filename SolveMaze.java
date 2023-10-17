b4142628718a622cc202eca89c8f6a90a8ef2229
mneedham01 <mneedham@smith.edu>
2023-10-17 16:37:03 -0400

    moved solve to maze class

--- SolveMaze.java
+++ SolveMaze.java
@@ -2,32 +2,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/** SolveMaze
+ * Main method reads in file and calls checkLocation
+*/
 class SolveMaze {
 
-  /** */
-  public boolean checkLocation (Maze maze, MazeLocation current) {
-    // if current is final location in maze
-    if (current.equals(maze.getFinish())) {
-      // mark current as part of path
-      maze.grid[current.getRow()][current.getCol()] = MazeContents.PATH;
-      return true;
-    }
-    MazeContents contentsAtCurrent = maze.getContents(current.getRow(), current.getCol());
-    // if current is a wall or already visited
-    if (contentsAtCurrent.equals(MazeContents.VISITED) || contentsAtCurrent.equals(MazeContents.WALL)) {
-      return false;
-    }
-    else {
-      maze.grid[current.getRow()][current.getCol()] = MazeContents.VISITED;;
-      try { Thread.sleep(50);	} catch (InterruptedException e) {};
-      if (checkLocation(maze, current.neighbor(MazeDirection.NORTH)) || checkLocation(maze, current.neighbor(MazeDirection.SOUTH)) || checkLocation(maze, current.neighbor(MazeDirection.EAST)) || checkLocation(maze, current.neighbor(MazeDirection.WEST))) {
-        maze.grid[current.getRow()][current.getCol()] = MazeContents.PATH;
-      }
-    }
-    return true;
-  }
-
-
+  /** Reads in a file from command line and solves maze */
   public static void main(String[] args) {
     Scanner file = null;
     if (args.length > 0) {
@@ -72,25 +52,25 @@ class SolveMaze {
     MazeLocation start = new MazeLocation(0,0);
     MazeLocation finish = new MazeLocation(0,0);
     while (file2.hasNextLine()) {
-      line = file.nextLine();
-      String value = String.valueOf(line.charAt(0));
+      line = file2.nextLine();
       for (int i = 0; i < columnCount; i++) {
+        String value = String.valueOf(line.charAt(i));
         // WALL
-        if (value == "#") {
+        if (value.equals("#")) {
           grid[rowCounter][i] = MazeContents.WALL;
         }
         // OPEN
-        if (value == ".") {
+        if (value.equals(".")) {
           grid[rowCounter][i] = MazeContents.OPEN;
         }
         // start
-        if (value == "S") {
+        if (value.equals("S")) {
           grid[rowCounter][i] = MazeContents.OPEN;
           start.setRow(rowCounter);
           start.setCol(i);
         }
         // finish
-        if (value == "F") {
+        if (value.equals("F")) {
           grid[rowCounter][i] = MazeContents.OPEN;
           finish.setRow(rowCounter);
           finish.setCol(i);
@@ -100,25 +80,14 @@ class SolveMaze {
     }
     file2.close();
 
+    System.out.println("finish = "+finish.getRow()+","+finish.getCol());
+
     // initalize a new maze
     Maze maze = new Maze(start, finish, grid);
     MazeViewer viewer = new MazeViewer(maze);
-    SolveMaze solve = new SolveMaze();
-    if (solve.checkLocation(maze, maze.getStart())) {
+    if (maze.solve(maze.getStart())) {
       System.out.println("Found solution");
     }
-
-
-
-
-
-
-    // Maze maze = new Maze();
-    // MazeViewer viewer = new MazeViewer(maze);
-    // SolveMaze solve = new SolveMaze();
-    // if (solve.checkLocation(maze, maze.getStart())) {
-    //   System.out.println("Found solution");
-    // }
 
   }
 }
